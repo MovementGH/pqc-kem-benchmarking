3176ebeb4c185a125ea1a06a900f5b3234b97f1f
William Furgerson <48330137+MovementGH@users.noreply.github.com>
2025-10-26 17:10:58 -0400

    Documentation

--- tests/benchmark.c
+++ tests/benchmark.c
@@ -2,8 +2,18 @@
 #include <unistd.h>
 #include "benchmark.h"
 
+/**
+ * This function measures the elapsed time for running a given function
+ * multiple times.
+ *
+ * @param func Pointer to the function that needs to be timed.
+ * @param runs Number of times the function should be called for measuring the time.
+ * @return Elapsed execution time in microseconds.
+ */
 int getTime(function_t func, int runs) {
     struct timeval start, end;
+
+    // Record the current time before running the function
     gettimeofday(&start, NULL);
 
     for(int i = 0; i < runs; i++)
@@ -11,28 +21,46 @@ int getTime(function_t func, int runs) {
 
     gettimeofday(&end, NULL);
 
+    // Calculate the elapsed time in microseconds
     return (end.tv_sec - start.tv_sec) * 1000 * 1000 + (end.tv_usec - start.tv_usec);
 }
 
+/**
+ * @brief Benchmarks a function over multiple runs and returns an average execution time.
+ *
+ * This function repeatedly calls the provided function until it achieves
+ * a benchmarking duration within a specified tolerance. It adjusts the number of runs
+ * dynamically based on the initial execution time to get as close as possible
+ * to the desired duration while making sure to meet a minimum run count.
+ *
+ *
+ * @param function Pointer to the function to be benchmarked.
+ * @param duration Desired benchmarking duration in seconds.
+ * @param minRuns Minimum number of runs allowed, even if it takes more than 'duration' seconds.
+ * @return Average execution time per call in microseconds.
+ */
 float benchmark(function_t function, int duration, int minRuns) {
     int runs = minRuns, runTime = 0;
+
     for(int i = 0; i < 10; i++) {
         runTime = getTime(function, runs);
 
-        // If we hit the desired benchmarking time, break
+        // Check if the measured time is within 5% of the desired duration
         if(runTime > duration * .95f && runTime < duration * 1.05f)
             break;
 
-        // If the function takes too long to run, return results from minRuns
+        // If the function takes too long to run, adjust the number of runs for subsequent tests
         runs *= (float)duration / (float)runTime;
-        
+
+        // Ensure that we don't fall below the minimum number of runs
         if(runs < minRuns) {
             runs = minRuns;
             break;
         }
     }
 
+    // Measure the time again with the final calculated number of runs
     runTime = getTime(function, runs);
-    
+
     return (float)runTime / (float)runs;
 }
--- tests/main.c
+++ tests/main.c
@@ -10,34 +10,54 @@
 
 unsigned char *sk, *pk, *ct, *ss;
 
+/**
+ * @brief Initializes the cryptographic keys and generates a new key pair.
+ */
 void setup() {
-  sk = calloc (CRYPTO_SECRETKEYBYTES, sizeof (unsigned char));
-  pk = calloc (CRYPTO_PUBLICKEYBYTES, sizeof (unsigned char));
-  ct = calloc (CRYPTO_CIPHERTEXTBYTES, sizeof (unsigned char));
-  ss = calloc (CRYPTO_BYTES, sizeof (unsigned char));
-  crypto_kem_keypair (pk, sk);
+    sk = calloc(CRYPTO_SECRETKEYBYTES, sizeof(unsigned char));
+    pk = calloc(CRYPTO_PUBLICKEYBYTES, sizeof(unsigned char));
+    ct = calloc(CRYPTO_CIPHERTEXTBYTES, sizeof(unsigned char));
+    ss = calloc(CRYPTO_BYTES, sizeof(unsigned char));
+
+    // Generate a new key pair
+    crypto_kem_keypair(pk, sk);
 }
 
+/**
+ * @brief Encapsulates a key using the KEM.
+ */
 void enc() {
-  crypto_kem_enc (ct, ss, pk);
+    crypto_kem_enc(ct, ss, pk);
 }
 
+/**
+ * @brief Decapsulates a key using the KEM
+ */
 void dec() {
-  crypto_kem_dec (ss, ct, sk);
+    crypto_kem_dec(ss, ct, sk);
 }
 
 int main(int argc, char** argv) {
+    // Initialize cryptographic keys
     setup();
+
+    // Print out the details of the KEM
     printf("Algorithm: %s\n", CRYPTO_ALGNAME);
     printf("Public Key Size: %d\n", CRYPTO_PUBLICKEYBYTES);
     printf("Private Key Size: %d\n", CRYPTO_SECRETKEYBYTES);
     printf("Cipher Size: %d\n", CRYPTO_CIPHERTEXTBYTES);
 
+    // Benchmark the encryption function
     float encTime = benchmark(enc, BENCHMARK_LENGTH, MIN_RUNS);
+
+    // Benchmark the decryption function
     float decTime = benchmark(dec, BENCHMARK_LENGTH, MIN_RUNS);
 
-    printf("Encode: %.2f/s\n", 1000000.f / encTime);
-    printf("Decode: %.2f/s\n", 1000000.f / decTime);
+    // Output the results of the benchmarks in human readable format
+    printf("Encode: %.2f/s\n", 1000000.0f / encTime);
+    printf("Decode: %.2f/s\n", 1000000.0f / decTime);
 
-    fprintf(stderr, "%s,%d,%d,%d,%.2f,%.2f\n", argv[0] + 2, CRYPTO_PUBLICKEYBYTES, CRYPTO_SECRETKEYBYTES, CRYPTO_CIPHERTEXTBYTES, 1000000.f / encTime, 1000000.f / decTime);
+    // stderr is captured into a csv, so print all the metrics in that format as well.
+    fprintf(stderr, "%s,%d,%d,%d,%.2f,%.2f\n", argv[0] + 2, CRYPTO_PUBLICKEYBYTES, CRYPTO_SECRETKEYBYTES, CRYPTO_CIPHERTEXTBYTES, 1000000.0f / encTime, 1000000.0f / decTime);
+
 }
